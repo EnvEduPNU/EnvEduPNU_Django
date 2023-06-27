e52f05e53c4eefc4eee71324f4f9a367de8fed6f
jshjshjshjsh <127467152+jshjshjshjsh@users.noreply.github.com>
2023-06-27 14:18:15 +0900

    #4-feat/실행 중이던 서버 중지 코드 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,17 @@
 REPOSITORY=/home/ubuntu/EnvEdu_Django
 cd $REPOSITORY
 
-touch test
+RUNNING_NAME='/usr/bin/python3 ./manage.py'
+
+CURRENT_PID=$(pgrep -f $RUNNING_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 종료할것 없음."
+else
+  echo "> kill -9 $CURRENT_PID"
+  sleep 5
+  kill -15 $CURRENT_PID
+fi
 
 python3 ./manage.py runserver
